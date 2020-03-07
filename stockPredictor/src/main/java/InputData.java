ef755fb1b0274f4f8b3190ef68f90395dd374f1f
arutfield <36383013+arutfield@users.noreply.github.com>
2020-03-07 17:15:30 -0500

    linearizing map works

--- stockPredictor/src/main/java/InputData.java
+++ stockPredictor/src/main/java/InputData.java
@@ -5,10 +5,7 @@ import java.util.*;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
@@ -19,11 +16,7 @@ public class InputData {
     final static Logger logger = Logger.getLogger(InputData.class);
     Map<Integer, Double> dowJonesClosingMap = new HashMap<Integer, Double>();
 
-    public InputData(String filename) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(1985, 1, 29);
-        startDate = calendar.getTime();
-
+    public InputData(String filenameDowData) {
 
         BufferedReader br = null;
         String line = "";
@@ -31,27 +24,41 @@ public class InputData {
 
         try {
 
-            br = new BufferedReader(new FileReader(filename));
+            br = new BufferedReader(new FileReader( filenameDowData));
+            int dateDifference = -1;
             while ((line = br.readLine()) != null) {
 
                 // use comma as separator
                 String[] data = line.split(cvsSplitBy);
                 String dateOfStock=data[0];
                 try {
+                    int prevDateDifference = dateDifference;
                     Date date1=new SimpleDateFormat("yyyy-MM-dd").parse(dateOfStock);
-                    int dateDifference = (int) ((date1.getTime()-startDate.getTime()) / (1000.0 * 60 * 60 *24));
+                    if (startDate == null) {
+                        startDate = new Date(date1.getTime()-DateUtil.DAY_MILLISECONDS/12);
+                        logger.debug("start date of dow - " + startDate.toString());
+
+                    }
+                    dateDifference = (int) ((date1.getTime()-startDate.getTime()) / (1000.0 * 60 * 60 *24));
                     dowJonesClosingMap.put(dateDifference, Double.valueOf(data[4]));
+                    logger.trace("added day " + dateDifference + " for " + date1.toString() + ", "
+                            + dowJonesClosingMap.get(dateDifference)
+                            + " to map");
+                    int dateRateOfChange = dateDifference - prevDateDifference;
+                    if ((dateRateOfChange > 4) || (dateRateOfChange == 2) || dateRateOfChange < 1) {
+                        logger.warn("unusual date difference gap of " + dateRateOfChange);
+                    }
                 }
                  catch (ParseException ex) {
                     continue;
                 }
 
-                System.out.println("Data [date= " + data[0] + " , close=" + data[4] + "]");
 
             }
+            linearizeGaps(dowJonesClosingMap);
 
         } catch (FileNotFoundException ex) {
-            logger.error("File " + filename + " not found: " + ex.getMessage());
+            logger.error("File " +  filenameDowData + " not found: " + ex.getMessage());
             ex.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
@@ -70,4 +77,22 @@ public class InputData {
         return dowJonesClosingMap;
     }
 
+    private void linearizeGaps(Map<Integer, Double> map) {
+        int lastKnownKey = 0;
+        for (int i=1; i<Collections.max(map.keySet()); i++) {
+            if (map.containsKey(i)) {
+                lastKnownKey = i;
+                continue;
+            }
+            int j=0;
+            while (!map.containsKey(i+j)) {
+                j++;
+            }
+            int daysBetween = j+1;
+            double slope = (map.get(i+j) - map.get(lastKnownKey))/daysBetween;
+            map.put(i, slope*(i-lastKnownKey)+map.get(lastKnownKey));
+            logger.trace("added day " + i + " with value " + map.get(i+j));
+            lastKnownKey = i;
+        }
+    }
 }
--- stockPredictor/src/test/java/InputDataTest.java
+++ stockPredictor/src/test/java/InputDataTest.java
@@ -20,8 +20,32 @@ public class InputDataTest {
     {
         System.out.println("current directory: " + System.getProperty("user.dir"));
         InputData inputData = new InputData("target\\classes\\DJI.csv");
-        assertEquals(inputData.getDowJonesClosingMap().size(), 8838);
+        assertTrue(inputData.getDowJonesClosingMap().size()> 8838);
     }
 
+    @Test
+    public void readDayZero()
+    {
+        System.out.println("current directory: " + System.getProperty("user.dir"));
+        InputData inputData = new InputData("target\\classes\\DJI.csv");
+        assertEquals(1292.619995, inputData.getDowJonesClosingMap().get(0), 1e-6);
+    }
+
+    @Test
+    public void readDayOneHundred()
+    {
+        System.out.println("current directory: " + System.getProperty("user.dir"));
+        InputData inputData = new InputData("target\\classes\\DJI.csv");
+        assertEquals(1260.27002, inputData.getDowJonesClosingMap().get(100), 1e-6);
+    }
+
+
+    @Test
+    public void predictDayFour()
+    {
+        System.out.println("current directory: " + System.getProperty("user.dir"));
+        InputData inputData = new InputData("target\\classes\\DJI.csv");
+        assertEquals(1281.839966, inputData.getDowJonesClosingMap().get(4), 1e-6);
+    }
 
 }
